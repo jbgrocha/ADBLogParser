adf83e0d0c1c8821166e246781fb330e52d5d8a6
jbgrocha <jbgrocha@LITTLEHEX>
2017-12-12 18:33:02 +0000

    unparsed Events

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ADBLogParser
+namespace LogParser
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ADBLogParser logParser = new ADBLogParser("..\\..\\Resources\\StrokesLog1.txt");
+            //logParser.printFileLines();
+            logParser.printUnparsedEvents();
         }
     }
 }
