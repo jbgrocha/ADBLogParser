0a895b10a32749d25653bfb14bb8002e38670f76
jbgrocha <jbgrocha@LITTLEHEX>
2017-12-12 22:30:33 +0000

    Added ADBLogEvent Class

---
+++ ADBLogEvent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBLogParser
+{
+    class ADBLogEvent
+    {
+        public double timestamp { get; set; }
+        public string device { get; set; }
+        public string opCode { get; set; }
+        public string eventType { get; set; }
+        public int eventValue { get; set; }
+
+        public const int TOUCH_UP = 0;
+        public const int TOUCH_DOWN = 1;
+
+        public ADBLogEvent(string[] eventText)
+        {
+            this.setTimestamp(eventText[0]);
+
+            this.device = eventText[1];
+
+            this.opCode = eventText[2];
+
+            this.eventType = eventText[3];
+            
+            this.setEventValue(eventText[4]);
+        }
+
+        private void setTimestamp(string timestampTxt)
+        {
+            this.timestamp = Math.Round(double.Parse(timestampTxt, CultureInfo.InvariantCulture.NumberFormat), 6);
+        }
+        
+        private void setEventValue(string eventValueTxt)
+        {
+            if ((this.eventType == "BTN_TOUCH") && (eventValueTxt == "UP"))
+            {
+                this.eventValue = TOUCH_UP;
+
+            } else if ((this.eventType == "BTN_TOUCH") && (eventValueTxt == "DOWN"))
+            {
+                this.eventValue = TOUCH_DOWN;
+
+            } else
+            {
+                this.eventValue = int.Parse(eventValueTxt, NumberStyles.HexNumber);
+            }
+        }
+    }
+}
