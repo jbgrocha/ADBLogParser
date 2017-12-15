1bef6b5ab4e35a3ae51f82843dbabfb26af30b3d
jbgrocha <jbgrocha@LITTLEHEX>
2017-12-15 11:53:18 +0000

    Added Stroke and Sample classes.

---
+++ Sample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBLogParser
+{
+    class Sample
+    {
+        public double TimeStamp { get; set; }
+        public Dictionary<string, int> Features { get; set; }
+
+        public Sample() {
+            TimeStamp = 0.0;
+            Features = new Dictionary<string, int>();
+        }
+
+        public Sample(double SampleTimeStamp)
+        {
+            TimeStamp = SampleTimeStamp;
+            Features = new Dictionary<string, int>();
+        }
+
+        public Sample(double SampleTimeStamp, Dictionary<string, int> SampleFeatures)
+        {
+            TimeStamp = SampleTimeStamp;
+            Features = SampleFeatures;
+        }
+    }
+}
---
+++ Stroke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBLogParser
+{
+    class Stroke
+    {
+        public List<Sample> Samples { get; set; }
+
+        public Stroke()
+        {
+            Samples = new List<Sample>();
+        }
+
+        public Stroke(List<Sample> StrokeSamples)
+        {
+            Samples = StrokeSamples;
+        }
+    }
+}
