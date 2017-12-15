be4f7fea479836e6b53e70406dc580b6817757b7
jbgrocha <jbgrocha@LITTLEHEX>
2017-12-15 16:00:10 +0000

    SingleTouchParser from scratch

---
+++ SingleTouchParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBLogParser
+{
+    class SingleTouchParser
+    {
+        private List<ADBLogEvent> Events { get; set; }
+        private List<Stroke> Strokes { get; set; }
+
+        private Stroke CurrentStroke;
+        private Sample CurrentSample;
+
+        public SingleTouchParser(List<ADBLogEvent> EventsToParse)
+        {
+            Events = EventsToParse;
+            Strokes = new List<Stroke>();
+
+        }
+    }
+}
