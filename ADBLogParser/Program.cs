2208ff5fec13b7179c1a5feee5dd4027469a1fe1
jbgrocha <jose.bernardo.rocha@gmail.com>
2017-12-17 21:35:57 +0000

    rollback

---
+++ ADBLogParser/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ADBLogParser logParser = new ADBLogParser("..\\..\\Resources\\SingleTouch\\01.txt");
+
+            //logParser.printFileLines();
+            //logParser.printUnparsedEvents();
+            //logParser.PrintParsedEvents();
+            //logParser.PrintFeatureSummary();
+            //logParser.PrintTouchSummary();
+
+            //PrintFeatureSummaryAllSessions();
+            //PrintFeatureSummarySingleTouchSessions();
+
+            //PrintTouchSummaryAllSessions();
+            //PrintTouchSummarySingleTouchSessions();
+
+            //PrintStrokes("..\\..\\Resources\\SingleTouch\\01.txt");
+            PrintJSONStrokes("..\\..\\Resources\\SingleTouch\\01.txt");
+        }
+
+        private static void PrintFeatureSummarySingleTouchSessions()
+        {
+            string targetDirectory = "..\\..\\Resources\\SingleTouch\\";
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+
+            Console.WriteLine("Feature Summary - Single Touch Sessions");
+            Console.WriteLine();
+
+            PrintFeatureSummaries(fileEntries);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintFeatureSummaryAllSessions()
+        {
+            string targetDirectory = "..\\..\\Resources\\All\\";
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+
+            Console.WriteLine("Feature Summary - All Sessions");
+            Console.WriteLine();
+
+            PrintFeatureSummaries(fileEntries);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintTouchSummarySingleTouchSessions()
+        {
+            string targetDirectory = "..\\..\\Resources\\SingleTouch\\";
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+
+            Console.WriteLine("Touch Summary - Single Touch Sessions");
+            Console.WriteLine();
+
+            PrintTouchSummaries(fileEntries);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintTouchSummaryAllSessions()
+        {
+            string targetDirectory = "..\\..\\Resources\\All\\";
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+
+            Console.WriteLine("Touch Summary - All Touch Sessions");
+            Console.WriteLine();
+
+            PrintTouchSummaries(fileEntries);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintFeatureSummaries(string[] fileEntries)
+        {
+            foreach (string fileEntry in fileEntries)
+            {
+                PrintFeatureSummary(fileEntry);
+            }
+        }
+
+        private static void PrintTouchSummaries(string[] fileEntries)
+        {
+            foreach (string fileEntry in fileEntries)
+            {
+                PrintTouchSummary(fileEntry);
+            }
+        }
+
+        private static void PrintFeatureSummary(string filePath)
+        {
+            Console.WriteLine(filePath);
+            ADBLogParser logParser = new ADBLogParser(filePath);
+            logParser.PrintFeatureSummary();
+            Console.WriteLine();
+        }
+
+        private static void PrintTouchSummary(string filePath)
+        {
+            Console.WriteLine(filePath);
+            ADBLogParser logParser = new ADBLogParser(filePath);
+            logParser.PrintTouchSummary();
+            Console.WriteLine();
+        }
+
+        private static void PrintStrokes(string filePath)
+        {
+            Console.WriteLine(filePath);
+            ADBLogParser logParser = new ADBLogParser(filePath);
+            logParser.PrintStrokes();
+        }
+
+        private static void PrintJSONStrokes(string filePath)
+        {
+            ADBLogParser logParser = new ADBLogParser(filePath);
+            Console.Write(logParser.StrokesToJSON());
+        }
+    }
+}
---
+++ ADBLogParser/SingleTouchParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Strokes;
+
+namespace ADBLogParser
+{
+    class SingleTouchParser
+    {
+        private List<ADBLogEvent> Events { get; set; }
+        public List<Stroke> Strokes { get; set; }
+
+        private Stroke CurrentStroke;
+        private Sample CurrentSample;
+
+        public SingleTouchParser(List<ADBLogEvent> EventsToParse)
+        {
+            Events = EventsToParse;
+            Strokes = new List<Stroke>();
+
+            CurrentStroke = null;
+            CurrentSample = null;
+
+            Parse();
+        }
+
+        private void Parse()
+        {
+            foreach (ADBLogEvent currentEvent in Events)
+            {
+
+                if ((currentEvent.EventType == "BTN_TOUCH") && (currentEvent.EventValue == ADBLogEvent.TOUCH_DOWN) && (CurrentStroke == null))
+                {
+                    // Start Stroke
+                    CurrentStroke = new Stroke();
+
+                } else if ((currentEvent.EventType == "BTN_TOUCH") && (currentEvent.EventValue == ADBLogEvent.TOUCH_UP) && (CurrentStroke != null))
+                {
+                    // Add Current Stroke to Strokes
+                    Strokes.Add(CurrentStroke);
+                    
+                    // End Stroke
+                    CurrentStroke = null;
+
+                } else if((CurrentSample == null) && (currentEvent.OpCode == "EV_ABS"))
+                {
+                    // Create Sample
+                    CurrentSample = new Sample(currentEvent.Timestamp);
+
+                    // Add Feature to Current Sample
+                    CurrentSample.AddFeature(currentEvent.EventType, currentEvent.EventValue);
+
+                } else if((CurrentSample != null) && (currentEvent.OpCode == "EV_ABS"))
+                {
+                    // Add Feature to Current Sample
+                    CurrentSample.AddFeature(currentEvent.EventType, currentEvent.EventValue);
+
+                } else if((CurrentSample != null) && (currentEvent.EventType == "SYN_MT_REPORT"))
+                {
+                    // Add Current Sample to Stroke
+                    CurrentStroke.Samples.Add(CurrentSample);
+
+                    // Set Current Sample to null
+                    CurrentSample = null;
+                }
+            }
+        }
+    }
+}
