4392ec084634995bebba01647031616fc5cfa232
jbgrocha <jose.bernardo.rocha@gmail.com>
2017-12-17 21:43:00 +0000

    Strokes added to solution

---
+++ Strokes/Sample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strokes
+{
+    public class Sample
+    {
+        public double TimeStamp { get; set; }
+        public Dictionary<string, int> Features { get; set; }
+
+        public Sample() {
+            TimeStamp = 0.0;
+            Features = new Dictionary<string, int>();
+        }
+
+        public Sample(double SampleTimeStamp)
+        {
+            TimeStamp = SampleTimeStamp;
+            Features = new Dictionary<string, int>();
+        }
+
+        public Sample(double SampleTimeStamp, Dictionary<string, int> SampleFeatures)
+        {
+            TimeStamp = SampleTimeStamp;
+            Features = SampleFeatures;
+        }
+
+        public void AddFeature(string key, int value)
+        {
+            if(!Features.ContainsKey(key))
+            {
+                Features.Add(key, value);
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "Sample - " + TimeStamp + "\n";
+
+            foreach(KeyValuePair<string, int> feature in Features)
+            {
+                result += feature.Key + " - " + feature.Value + "\n";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Strokes/Stroke.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strokes
+{
+    public class Stroke
+    {
+        public List<Sample> Samples { get; set; }
+
+        public Stroke()
+        {
+            Samples = new List<Sample>();
+        }
+
+        public Stroke(List<Sample> StrokeSamples)
+        {
+            Samples = StrokeSamples;
+        }
+
+        public override string ToString()
+        {
+            string result = "Stroke\n";
+            foreach (Sample sample in Samples)
+            {
+                result += sample;
+            }
+
+            return result;
+        }
+    }
+}
