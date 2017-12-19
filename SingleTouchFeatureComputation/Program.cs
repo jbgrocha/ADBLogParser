7a01a51c7a1c8770973a0c1720d9324570b0d8e2
jbgrocha <jose.bernardo.rocha@gmail.com>
2017-12-19 12:50:08 +0000

    Average test

--- SingleTouchFeatureComputation/Program.cs
+++ SingleTouchFeatureComputation/Program.cs
@@ -12,7 +12,8 @@ namespace SingleTouchFeatureComputation
     {
         public static void Main(string[] args)
         {
-            PrintSessionToJSON("..\\..\\Resources\\Session-01.json");
+            //PrintSessionToJSON("..\\..\\Resources\\Session-01.json");
+            Average_X("..\\..\\Resources\\Session-01.json");
         }
 
         private static void PrintSessionToJSON(string filePath)
@@ -25,6 +26,25 @@ namespace SingleTouchFeatureComputation
         {
             SessionParser sessionParser = new SessionParser(filePath);
             return sessionParser.Session;
+        }
+
+        private static void Average_X(string filePath)
+        {
+            Session session = ReadSession(filePath);
+
+            List<double> averages = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                double avg_x = stroke.GetFeatureValuesFromSamples("ABS_MT_POSITION_X").Average();
+                averages.Add(avg_x);
+            }
+
+            Console.WriteLine("Number of Strokes: " + session.Strokes.Count);
+
+            foreach(double average in averages) {
+                Console.WriteLine(average);
+            }
         }
     }
 }
