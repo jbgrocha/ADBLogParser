7f702ba1991512e69e15a46e3b01a250586b2714
jbgrocha <jose.bernardo.rocha@gmail.com>
2017-12-27 14:29:00 +0000

    refactoring

---
+++ SingleTouchFeatureComputation/Features.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Statistics;
+using Sessions;
+
+namespace SingleTouchFeatureComputation
+{
+    class Features
+    {
+        private static string X = "ABS_MT_POSITION_X";
+        private static string Y = "ABS_MT_POSITION_Y";
+
+        // X features
+        public static List<double> Avg_X(Session session)
+        {
+            return Avg(session, X);
+        }
+
+        public static List<double> Min_X(Session session)
+        {
+            return Min(session, X);
+        }
+
+        public static List<double> Max_X(Session session)
+        {
+            return Max(session, X);
+        }
+
+        public static List<double> StandardDeviation_X(Session session)
+        {
+            return StandardDeviation(session, X);
+        }
+
+        //Y features
+        public static List<double> Avg_Y(Session session)
+        {
+            return Avg(session, Y);
+        }
+
+        public static List<double> Min_Y(Session session)
+        {
+            return Min(session, Y);
+        }
+
+        public static List<double> Max_Y(Session session)
+        {
+            return Max(session, Y);
+        }
+
+        public static List<double> StandardDeviation_Y(Session session)
+        {
+            return StandardDeviation(session, Y);
+        }
+
+        //  ABS_MT_TOUCH_MAJOR / ABS_MT_WIDTH_MAJOR -> Touch and width relationship -> mojgan called this pressure
+
+        // lengthT -> sum of pitagorean propositions -> layman's version -> sum of the distance between the samples
+
+        // spanX and span Y -> total span in direction of x (probably max - min)
+
+        // distance X and distance Y  -> distance from start to end (probably start.x - end.x)
+
+        // displacement
+
+        //Aux Stat
+        public static List<double> Avg(Session session, string feature)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
+                double avg = currentFeature.Average();
+                result.Add(avg);
+            }
+
+            return result;
+        }
+
+        public static List<double> Min(Session session, string feature)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
+                double min = currentFeature.Min();
+                result.Add(min);
+            }
+
+            return result;
+        }
+
+        public static List<double> Max(Session session, string feature)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
+                double max = currentFeature.Max();
+                result.Add(max);
+            }
+
+            return result;
+        }
+
+        public static List<double> StandardDeviation(Session session, string feature)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
+                double std = Measures.StandardDeviation(currentFeature, currentFeature.Mean());
+                result.Add(std);
+            }
+
+            return result;
+        }
+
+        public static List<double> Median(Session session, string feature)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
+                double median = Measures.Median(currentFeature);
+                result.Add(median);
+            }
+
+            return result;
+        }
+
+        //Other Features
+        public static List<double> Duration(Session session)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                result.Add(Duration(stroke));
+            }
+
+            return result;
+        }
+
+        public static double Duration(Stroke stroke)
+        {
+            double result = stroke.EndTime - stroke.StartTime;
+            return result;
+        }
+
+        public static List<double> Dist2Prev(Session session)
+        {
+
+            List<double> result = new List<double>();
+
+            double previousEnd = 0.0;
+            double currentStart = 0.0;
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                currentStart = stroke.StartTime;
+
+                result.Add(currentStart - previousEnd);
+
+                previousEnd = stroke.EndTime;
+            }
+
+            return result;
+        }
+
+        public static List<double> TimeElapsed(Session session)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (Stroke stroke in session.Strokes)
+            {
+                double currentStart = stroke.StartTime;
+
+                double sessionStartTime = session.Strokes.First<Stroke>().StartTime;
+
+                result.Add(currentStart - sessionStartTime);
+            }
+
+            return result;
+        }
+    }
+}
--- SingleTouchFeatureComputation/Program.cs
+++ SingleTouchFeatureComputation/Program.cs
@@ -1,5 +1,4 @@
-﻿using Accord.Statistics;
-using Sessions;
+﻿using Sessions;
 using SingleTouchSessionParser;
 using System;
 using System.Collections.Generic;
@@ -9,214 +8,20 @@ namespace SingleTouchFeatureComputation
 {
     class Program
     {
-        private static string X = "ABS_MT_POSITION_X";
-        private static string Y = "ABS_MT_POSITION_Y";
-
-
         public static void Main(string[] args)
         {
             //PrintSessionToJSON("..\\..\\Resources\\Session-01.json");
             //List<double> test = Median("..\\..\\Resources\\Session-01.json", "ABS_MT_POSITION_X");
-            List<double> test = TimeElapsed("..\\..\\Resources\\Session-01.json");
+
+            Session session = ReadSession("..\\..\\Resources\\Session-01.json");
+
+            List<double> test = Features.TimeElapsed(session);
 
             foreach (double value in test)
             {
                 Console.WriteLine(value);
             }
 
-        }
-
-        // X features
-        private static List<double> Avg_X(string filePath)
-        {
-            return Avg(filePath, X);
-        }
-
-        private static List<double> Min_X(string filePath)
-        {
-            return Min(filePath, X);
-        }
-
-        private static List<double> Max_X(string filePath)
-        {
-            return Max(filePath, X);
-        }
-
-        private static List<double> StandardDeviation_X(string filePath)
-        {
-            return StandardDeviation(filePath, X);
-        }
-
-        //Y features
-        private static List<double> Avg_Y(string filePath)
-        {
-            return Avg(filePath, Y);
-        }
-
-        private static List<double> Min_Y(string filePath)
-        {
-            return Min(filePath, Y);
-        }
-
-        private static List<double> Max_Y(string filePath)
-        {
-            return Max(filePath, Y);
-        }
-
-        private static List<double> StandardDeviation_Y(string filePath)
-        {
-            return StandardDeviation(filePath, Y);
-        }
-
-        //  ABS_MT_TOUCH_MAJOR / ABS_MT_WIDTH_MAJOR -> Touch and width relationship -> mojgan called this pressure
-
-        // lengthT -> sum of pitagorean propositions -> layman's version -> sum of the distance between the samples
-
-        // spanX and span Y -> total span in direction of x (probably max - min)
-
-        // distance X and distance Y  -> distance from start to end (probably start.x - end.x)
-
-        // displacement
-
-        //Aux Stat
-        private static List<double> Avg(string filePath, string feature)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
-                double avg = currentFeature.Average();
-                result.Add(avg);
-            }
-
-            return result;
-        }
-
-        private static List<double> Min(string filePath, string feature)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
-                double min = currentFeature.Min();
-                result.Add(min);
-            }
-
-            return result;
-        }
-
-        private static List<double> Max(string filePath, string feature)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
-                double max = currentFeature.Max();
-                result.Add(max);
-            }
-
-            return result;
-        }
-
-        private static List<double> StandardDeviation(string filePath, string feature)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
-                double std = Measures.StandardDeviation(currentFeature, currentFeature.Mean());
-                result.Add(std);
-            }
-
-            return result;
-        }
-
-        private static List<double> Median(string filePath, string feature)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                int[] currentFeature = stroke.GetFeatureValuesFromSamples(feature).ToArray();
-                double median = Measures.Median(currentFeature);
-                result.Add(median);
-            }
-
-            return result;
-        }
-
-        //Other Features
-        private static List<double> Duration(string filePath)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                result.Add(Duration(stroke));
-            }
-
-            return result;
-        }
-
-        private static double Duration(Stroke stroke)
-        {
-            double result = stroke.EndTime - stroke.StartTime;
-            return result;
-        }
-
-        private static List<double> Dist2Prev(string filePath)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            double previousEnd = 0.0;
-            double currentStart = 0.0;
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                currentStart = stroke.StartTime;
-
-                result.Add(currentStart - previousEnd);
-
-                previousEnd = stroke.EndTime;
-            }
-
-            return result;
-        }
-
-        private static List<double> TimeElapsed(string filePath)
-        {
-            Session session = ReadSession(filePath);
-
-            List<double> result = new List<double>();
-
-            foreach (Stroke stroke in session.Strokes)
-            {
-                double currentStart = stroke.StartTime;
-
-                double sessionStartTime = session.Strokes.First<Stroke>().StartTime;
-
-                result.Add(currentStart - sessionStartTime);
-            }
-
-            return result;
         }
 
         //Generic operations
